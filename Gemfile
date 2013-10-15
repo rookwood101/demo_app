cce7e1055007413bb910a09018284aa0a847978d
Alex Sadler <alexandersadler1@gmail.com>
2013-10-15 09:21:28 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'https://rubygems.org'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.0.0'
+
+group :development do
+	gem 'sqlite3'
+end
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+	gem 'sass-rails',   '~> 4.0.0'
+	gem 'coffee-rails', '~> 4.0.0'
+
+	gem 'uglifier', '~> 1.0.3'
+end
+
+gem 'jquery-rails'
+
+group :production do
+	gem 'pg', '0.17.0'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'ef7ab9ad059ffc0e6a1ec98bfc6fa739851c0d9694ce1e46e52b3b8fabe33ac3c940ead7e31e8e22ad54e4bd61bf39411ce33b66a4947495c86af4be69fc22d5'
